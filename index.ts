df5c4f0b4c78767c656754ae1df2737ce0efa3d8
Raphael Marandet <raphael.marandet@gmail.com>
2025-09-24 15:04:12 +0200

    there was no index.ts !

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from './src/Collection';
+export * from './src/Cursor';
+export * from './src/string-normalizer';
+export * from './src/storage-adapters/MemoryStorage';
+export * from './src/storage-adapters/DiskStorage';
+export * from './src/interfaces/IStorage';
+export * from './src/enums';
